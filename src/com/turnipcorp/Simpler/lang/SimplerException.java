f72bc1be5d4c6c8825bdd6c1f2cee3617bb4ec8e
frank-bisb <frank_dai@bisboston.org>
2024-09-09 09:06:14 -0400

    Copying code - lang/SimplerException

---
+++ src/com/turnipcorp/Simpler/lang/SimplerException.java
@@ -0,0 +1,16 @@
+package com.turnipcorp.Simpler.lang;
+
+/**
+ * Basically just a syntax error at this point
+ */
+public class SimplerException extends java.lang.Exception {
+
+	/**
+	 * Serial Version ID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SimplerException(String message) {
+		super(message);
+	}
+}
