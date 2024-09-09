b7cf4f94da9cf371df33c71ac485e117d967651c
frank-bisb <frank_dai@bisboston.org>
2024-09-09 09:06:51 -0400

    Copying code - lang/TerminatedError

---
+++ src/com/turnipcorp/Simpler/lang/TerminatedError.java
@@ -0,0 +1,38 @@
+package com.turnipcorp.Simpler.lang;
+
+public class TerminatedError extends java.lang.Error {
+
+	/**
+	 * Serial Version ID
+	 */
+	private static final long serialVersionUID = 0x7FFFFFL;
+	
+	/**
+	 * Error message
+	 * @see #getMessage()
+	 */
+	private String msg;
+	
+	/**
+	 * Constructor for InterpreterError class
+	 * @param arg message: String
+	 */
+	public TerminatedError(String arg) {super(arg); msg = arg;}
+	
+	/**
+	 * Returns the message given in the constructor
+	 */
+	@Override
+	public String getMessage() {
+		return msg;
+	}
+	
+	/**
+	 * calls Throwable.printStackTrace on System.out
+	 * @see #Throwable.printStackTrace(PrintStream)
+	 */
+	@Override
+	public void printStackTrace() {
+		super.printStackTrace(System.out);
+	}
+}
